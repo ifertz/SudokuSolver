95962af6a8beb26871120cd12ed03bae38b102cd
ifertz <31354303+ifertz@users.noreply.github.com>
2020-11-29 02:56:41 -0300

    Update AlgoritmoTCC.py

--- AlgoritmoTCC.py
+++ AlgoritmoTCC.py
@@ -43,7 +43,6 @@ def get_cells_from_indexes(grid, indexes):
 
 def solve(problem_grid, population_size=1000, selection_rate=0.5, max_generations_count=1000, mutation_rate=0.05):
 
-    # square root of the problem grid's size
     N = int(sqrt(len(problem_grid)))
 
     def empty_grid(elem_generator=None):
@@ -122,77 +121,38 @@ def solve(problem_grid, population_size=1000, selection_rate=0.5, max_generation
     def generate_initial_population():
         
         candidates = []
-        #print(candidates)
         for k in range(population_size):
             candidate = empty_grid()
             for i in range(len(problem_grid)):
                 shuffled_sub_grid = [n for n in range(1, len(problem_grid) + 1)]
                 shuffle(shuffled_sub_grid)
-                
-                #print(shuffled_sub_grid)
-                #print("\n")
 
                 for j in range(len(problem_grid)):
                     if problem_grid[i][j] is not None:
                         candidate[i][j] = problem_grid[i][j]
                         
                         shuffled_sub_grid.remove(problem_grid[i][j])
-                        #print(shuffled_sub_grid)
-                    #print(candidate)
-                    #print("\n")
-                #print(shuffled_sub_grid)
                 for j in range(len(problem_grid)):
                     if candidate[i][j] is None:
-                        #print(shuffled_sub_grid)
                         candidate[i][j] = shuffled_sub_grid.pop()
-                        #print(shuffled_sub_grid)
-                        #print("\n")
-                        #print(candidate)
-                #print("\n")
             candidates.append(candidate)
-            #print("\n\n\n\n\n")
-            #print("POPULAÇÃO INICIAL GERADA")
-            #print("\n\n\n\n\n")
         return candidates
 
     def fitness(grid):
         row_duplicates_count = 0
-        #print("PRINT DA GRID \n\n")
-        #print(grid)
         for i in range (len(grid)):
             row_duplicates_count += len(grid[i]) - len(set(grid[i]))
-            #print(grid[i])
-        #print("\n\n")
-        #print("len subgrid_duplicates_count:")
-        #print(row_duplicates_count)
         columns = []
         for a, b in same_column_indexes(problem_grid, 0, 0, N):
             row = list(get_cells_from_indexes(grid, same_row_indexes(problem_grid, a, b, N)))
             columns.append(row)
             #print(len(row) - len(set(row)))
             row_duplicates_count += len(row) - len(set(row))
-        #print("len row_duplicates_count:")
-        #print(row_duplicates_count)
-        #print("\n\n")
-        #print(columns)
         columns = list(map(list, zip(*columns)))
         
-        #print("\n antes")
-        #print(row_duplicates_count)
         for i in range (len(columns)):
             row_duplicates_count += len(columns[i]) - len(set(columns[i]))
         
-        #print("\n depois")
-        #print(row_duplicates_count)
-        #print("\n")
-        #print(columns)
-        #for i in range (len(grid)):
-        #    for j in range (len(grid[i])):
-        #        columns[i].append(tuple(grid[i][j]))
-        #print(columns)
-        
-        #print("len column_duplicates_count:")
-        #print(row_duplicates_count)
         return row_duplicates_count
     
     def probability(prob):
@@ -207,15 +167,12 @@ def solve(problem_grid, population_size=1000, selection_rate=0.5, max_generation
         index_fitness = []
         for i in range(len(candidates)):
             index_fitness.append(tuple([i, fitness(candidates[i])]))
-        #print(index_fitness)
 
         index_fitness.sort(key=lambda elem: elem[1])
 
         selected_part = index_fitness[0: int(len(index_fitness))]
-        #print(selected_part)
         indexes   = [e[0] for e in selected_part]
         fitnesses = [e[1] for e in selected_part]
-        #print(indexes)
         sum_fitness = sum(e[1] for e in selected_part)
         
         if sum_fitness == 0:
@@ -229,11 +186,8 @@ def solve(problem_grid, population_size=1000, selection_rate=0.5, max_generation
             chance = [
                 (1/(1))
                 /sum_fitness for e in selected_part]
-        #print(chance)
         
         prob = probability(chance)
-        
-        #print(prob)
         
         return [candidates[i] for i in indexes], selected_part[0][1], prob
     
@@ -242,38 +196,26 @@ def solve(problem_grid, population_size=1000, selection_rate=0.5, max_generation
     fill_predetermined_cells()
 
     population = generate_initial_population()
-    #print(population[0])
     best_fitness = None
     
     for generation in range(max_generations_count):
         population, best_fitness, prob = selection(population)
         
-        #print("\n")
-        #print(generation)
-        #print(population)
-        #print(prob)
-        #print(fitness(population[0]))
-
         if generation == max_generations_count - 1 or fitness(population[0]) == 0:
             break
-
-        #shuffle(population)
         
         new_population = []
         count = 0
+        
+        # CROSSOVER
         
         while True:
             solution_1, solution_2 = None, None
             if(len(population) <= 1):
                 break
             
-            #print(population)
-            #population.pop(npr.choice(len(population),p= prob))
-            #print("\n\n\n\n")
-            #print(population)
             try:
                 num = npr.choice(len(population),p= prob)
-                #print(num)
                 
                 solution_1 = population.pop(num)
                 prob.pop(num)
@@ -284,102 +226,39 @@ def solve(problem_grid, population_size=1000, selection_rate=0.5, max_generation
 
             try:
                 num = npr.choice(len(population),p= prob)
-                #print(num)
-                #print(len(population))
-                #print("\n")
                 solution_2 = population.pop()
                 prob.pop(num)
                 prob = probability(prob)
                 
             except IndexError:
                 break
-            #print("\n\n")
-            #print(solution_1)
-            #print(solution_2)
-            #new_population.append(solution_1)
-            #new_population.append(solution_2)
             
             luck_number = random.random()
             if(luck_number >= (1 - selection_rate)):
                 cross_point = randint(0, len(problem_grid) - 2)
-            #print(cross_point)
                 temp_sub_grid = solution_1[cross_point]
                 solution_1[cross_point] = solution_2[cross_point + 1]
                 solution_2[cross_point + 1] = temp_sub_grid
-            #print("\n")
-            #print(solution_1)
-            #print(solution_2)
             
             new_population.append(solution_1)
             new_population.append(solution_2)
-            #try:
-            #    population.pop()
-            #except IndexError:
-            #    break
-            #try:
-            #    population.pop()
-            #except IndexError:
-            #    break
-            #print("\n\n")
-            #print(new_population)
             
-        #print("randomizacao feita")
-        # mutation
+        # MUTATION
         
         for candidate in new_population[0:len(new_population)]:
-            #print(candidate)
-            #print(fitness(candidate))
-            #print("\n")
             for sub_grid_index in candidate[0:len(candidate)]:
-#               for cel_index in sub_grid_index[0:len(sub_grid_index)]:
-                #print(sub_grid_index)
-                #print(len(sub_grid_index))
                 for i, val in enumerate(sub_grid_index):
-                    #print("\n")
-                    #print(i)
-                    #print("\n")
                     luck_number = random.random()
                     if(luck_number <= mutation_rate):
                         for j, val2 in enumerate(sub_grid_index):
-                            #print("\n")
-                                #print(j+1)
                             tmp = copy.copy(candidate)
-                            #print("\n")
-                            #print(tmp[i])
                             if(i == j):
                                 continue
-                            #print("\n")
-                            #print(val)
-                            #print(val2)
                             fittmp = fitness(tmp)
-                            #print(fittmp)
                             sub_grid_index[i], sub_grid_index[j] = sub_grid_index[j], sub_grid_index[i]
-                            #print(sub_grid_index)
                             fitcand = fitness(candidate)
-                            #print(fitcand)
                             if(fittmp < fitcand):
-                                #print("teste")
                                 sub_grid_index[i], sub_grid_index[j] = sub_grid_index[j], sub_grid_index[i]
-                            #print(sub_grid_index)
-                            #print(fitness(candidate))
-            #print(candidate)
-            #print(fitness(candidate))
-            #print("\n\n\n")
-                        
-        #for candidate in new_population[0:int(len(new_population) * mutation_rate)]:
-        #    random_sub_grid = randint(0, 8)
-        #    possible_swaps = []
-        #    for grid_element_index in range(len(problem_grid)):
-        #        if problem_grid[random_sub_grid][grid_element_index] is None:
-        #            possible_swaps.append(grid_element_index)
-        #    if len(possible_swaps) > 1:
-        #        shuffle(possible_swaps)
-        #        first_index = possible_swaps.pop()
-        #        second_index = possible_swaps.pop()
-        #        tmp = candidate[random_sub_grid][first_index]
-        #        candidate[random_sub_grid][first_index] = candidate[random_sub_grid][second_index]
-        #        candidate[random_sub_grid][second_index] = tmp
-        ##print("mutacao feita")
         population.extend(new_population)
 
         
